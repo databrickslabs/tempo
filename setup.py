bf8534042a6db8df17a6e328093c00ad5362cfdb
Ricardo Portilla <ricardo.portilla@databricks.com>
2020-07-14 11:54:58 -0400

    initial commit from tsfinancial

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from setuptools import find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='tca',  
+     version='0.1',
+     author='Ricardo Portilla',
+     author_email='ricardo.portilla@databricks.com',
+     description='scalable python time series utility package',
+     long_description=long_description,
+   long_description_content_type='text/markdown',
+     url='https://github.com/rportilla-databricks/tsfinancial',
+     packages=find_packages(),
+     classifiers=[
+         'Programming Language :: Python :: 3',
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: OS Independent',
+     ],
+ )
