3daedb8e77dd4c06df4ed5bcaacb57f844924d92
Ricardo Portilla <ricardo.portilla@databricks.com>
2020-08-11 00:33:57 -0400

    add alias for right df in asof join

--- tca/base.py
+++ tca/base.py
@@ -63,14 +63,16 @@ class newBaseTs:
                     (col("is_original") == lit(1))) # Remove the overlapping partition parts in case we made use of time-partitions.
             .select(partitionCols + [ts_select_cols[0],ts_select_cols[1]]))
   
-  def asofJoin(self, right_DF, right_ts_col_name = None, partitionCols = [], tsPartitionVal = None, fraction = 0.5):    
+  def asofJoin(self, right_DF, right_ts_col_name = None, partitionCols = [], tsPartitionVal = None, fraction = 0.5, asof_prefix='asof'):    
     
     right_ts_col_name = self.ts_col if right_ts_col_name is None else right_ts_col_name
     
     # Define timeColumns that we will use throughout the calculation of the asof Join
     ts_select_cols = ["_".join([self.ts_col,val]) for val in ["left","right","ms"]] 
     right_DF = right_DF.withColumnRenamed(right_ts_col_name,self.ts_col)
-    
+   
+    # in order to avoid duplicates, we'll supply a prefix 
+    right_DF = right_DF.toDF(*(asof_prefix+'_'+c if c not in list(set().union(partitionCols,ts_select_cols, list(right_ts_col_name)) else c for c in right_DF.columns))
     unionDF = self.__getUnionDF(right_DF.withColumnRenamed(right_ts_col_name,self.ts_col),ts_select_cols,partitionCols)
     
     # Only make use of  time partitions if tsPartitionVal was supplied
