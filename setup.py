807bd5d974408c9aba40315a25c14c3913551e53
Tristan Nixon <tristan.nixon@databricks.com>
2020-09-29 16:24:39 -0700

    fixing find_packages call to get tempo package from current dir

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/databrickslabs/tempo',
-    packages=find_packages(where="tempo"),
+    packages=find_packages(where=".", include=["tempo"]),
     extras_require=dict(tests=["pytest"]),
     classifiers=[
         'Programming Language :: Python :: 3',
