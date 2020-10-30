c0846cd3c1a3e42d505592aea16f3880286fee0b
Ricardo Portilla <ricardo.portilla@databricks.com>
2020-10-29 22:10:00 -0400

    adding sequence sort to all windows (as of join needs the sequence number from the right-hand DF)

--- tempo/tsdf.py
+++ tempo/tsdf.py
@@ -182,7 +182,11 @@ class TSDF:
 
 
   def __baseWindow(self):
-    w = Window().orderBy(f.col(self.ts_col).cast("long"))
+    # add all sort keys - time is first, unique sequence number breaks the tie
+    ptntl_sort_keys = [self.ts_col, self.seq_nb]
+    sort_keys = [f.col(col_name).cast("long") for col_name in ptntl_sort_keys if col_name != '']
+
+    w = Window().orderBy(sort_keys)
     if self.partitionCols:
       w = w.partitionBy([f.col(elem) for elem in self.partitionCols])
     return w
@@ -337,23 +341,3 @@ class TSDF:
           summary_df = selected_df.select(*selected_df.columns, *derivedCols)
 
           return TSDF(summary_df, self.ts_col, self.partitionCols)
-
-  def toDelta(self, optimizationCols):
-      df = self.df
-      ts_col = self.ts_col
-      partitionCols = self.partitionCols
-      optimizationCols = optimizationCols + ['event_time']
-      local = False
-      try:
-          dbutils.fs.ls("/")
-          True
-      except:
-          'Running in local mode'
-
-      format = "parquet" if local else "delta"
-      view_df = df.withColumn("event_dt", f.to_date(col(ts_col))) \
-          .withColumn("event_time", f.translate(f.split(f.col(ts_col).cast("string"), ' ')[1], ':', '').cast("double"))
-      view_df.write.mode("overwrite").partitionBy("event_dt").format(format).saveAsTable(tab_name)
-
-      if not local:
-          spark.sql("optimize {} zorder by {}".format(tab_name, "(" + ",".join(partitionCols + optimizationCols) + ")"))
