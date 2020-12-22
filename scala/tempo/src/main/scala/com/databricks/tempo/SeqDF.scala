9bd8b5eab5dc9ee5e5666d47c8c2a7e8016bcb89
Tristan Nixon <tristan.nixon@databricks.com>
2020-12-21 16:36:36 -0800

    Refactored code into both: * SeqDF - Sequenced DataFrames * TSDF - Timeseries DataFrames

---
+++ scala/tempo/src/main/scala/com/databricks/tempo/SeqDF.scala
@@ -0,0 +1,302 @@
+package com.databricks.tempo
+
+import org.apache.spark.ml.PipelineStage
+import org.apache.spark.ml.param.{Param, ParamMap, StringArrayParam}
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.expressions.{Window, WindowSpec}
+import org.apache.spark.sql.functions.row_number
+import org.apache.spark.sql.types._
+
+/**
+ * A DataFrame ordered by an explicit sequencing column
+ *
+ * @constructor create a new [[SeqDF]] instance
+ * @param sourceDF the source [[DataFrame]]
+ * @param totalOrderingCol the sequence column (must be of an [[org.apache.spark.sql.types.IntegralType]])
+ * @param sequencingColumns other sequence columns (used to generate the total ordering)
+ * @param partitionCols columns used to distinguish different sequences from one another
+ */
+class SeqDF(sourceDF: DataFrame,
+            val totalOrderingCol: String,
+            val sequencingColumns: Seq[String],
+            val partitionCols: Seq[String] )
+{
+	/** Validate arguments */
+	private val columnValidator = SeqDF.columnValidator(sourceDF.schema) _
+
+	// the source dataframe should not contain the total ordering column
+	assert( ! sourceDF.schema.fieldNames.map(_.toLowerCase).contains(totalOrderingCol.toLowerCase),
+	        s"Source DataFrame already contains a column named $totalOrderingCol!" )
+
+	// validate sequencing columns
+	private val seqColValidator = columnValidator("sequencing",Seq())(_)
+	assert( sequencingColumns.nonEmpty, "There must be at least one sequencing column!" )
+	sequencingColumns.foreach(seqColValidator)
+
+	// validate the partition columns
+	private val partitionValidator = columnValidator("partition",Seq())(_)
+	partitionCols.foreach( partitionValidator )
+
+	/** Generate total ordering over the DataFrame */
+
+	// define window for ordering according to the combined sequence columns
+	private val to_win =
+		if( partitionCols.nonEmpty)
+			Window.partitionBy(partitionCols.map(sourceDF(_)) :_*)
+			      .orderBy(sequencingColumns.map(sourceDF(_)) :_*)
+		else
+			Window.orderBy(sequencingColumns.map(sourceDF(_)) :_*)
+
+	// add total ordering column
+	val df = sourceDF.withColumn(totalOrderingCol, row_number().cast(LongType).over(to_win))
+
+	/** Calculate other properties */
+
+	// if the SeqDF is partitioned or not
+	val isPartitioned: Boolean = partitionCols.nonEmpty
+
+	// take a note of the observation columns (not sequence or partition)
+	val observationColumns: Seq[String] = df.schema.fieldNames.filter(col => {
+		val lc_col = col.toLowerCase
+		( lc_col != totalOrderingCol.toLowerCase &&
+		  !sequencingColumns.map(_.toLowerCase).contains(lc_col) &&
+		  ! partitionCols.map(_.toLowerCase).contains(lc_col))
+	})
+
+	// take note of measure columns (that have a numeric type)
+	val measureColumns: Seq[String] = observationColumns.filter(col => df.schema
+	                                                                     .find(_.name.toLowerCase == col.toLowerCase)
+	                                                                     .get.dataType.isInstanceOf[NumericType])
+
+	/** Helper Constructors */
+
+	/**
+	 * @constructor Create a new instance of [[SeqDF]]
+	 * @param sourceDF the source [[DataFrame]]]
+	 * @param sequencingCols sequence columns that give the total ordering, in order of decreasing precedence
+	 * @param partitionCols columns used to distinguish different sequences from one another
+	 */
+	def this(sourceDF: DataFrame, sequencingCols: Seq[String], partitionCols: Seq[String]) =
+		this( sourceDF,
+		      SeqDF.GENERATED_TOTAL_ORDERING_COL,
+		      sequencingCols,
+		      partitionCols)
+
+	/**
+	 * Use the given column names to define a combined total ordering over the SeqDF
+	 * @param sequencingCols the columns, in order of decreasing precedence, over which to define a total ordering
+	 * @return a new SeqDF instance, with total ordering defined by the given columns in decreasing precedence
+	 */
+	def orderedBy(sequencingCols: String*): SeqDF =
+		new SeqDF(this.df, this.totalOrderingCol, sequencingCols, this.partitionCols)
+
+	/**
+	 * Add or modify partition columns
+	 * @param partitionCols the names of columns used to partition the SeqDF
+	 * @return a new SeqDF instance, partitioned according to the given columns
+	 */
+	def partitionedBy(partitionCols: String*): SeqDF =
+		new SeqDF(this.df, this.totalOrderingCol, this.sequencingColumns, partitionCols)
+
+	/** Window Builders */
+
+	/**
+	 * Construct a base window for this SeqDF
+	 * @return a window designed to operate on this SeqDF
+	 */
+	protected def baseWindow(): WindowSpec =
+	{
+		val w = Window.orderBy(totalOrderingCol)
+
+		// partition if needed
+		if( this.isPartitioned )
+			w.partitionBy(partitionCols.head, partitionCols.tail :_*)
+		else
+			w
+	}
+
+	/**
+	 * Construct a window for this SeqDF
+	 * @param start start row for the window
+	 * @param end end row for the window
+	 * @return a window appropriate for applying functions to this SeqDF
+	 */
+	def rowsBetweenWindow(start: Long, end: Long): WindowSpec =
+	{
+		assert(end > start)
+		baseWindow().rowsBetween(start, end)
+	}
+
+	/**
+	 * Construct a leading window for this SeqDF
+	 * @param size the size (in number of sequential observations) of the window
+	 * @param includesNow whether the window should include the current observation
+	 * @return a window appropriate for calculating leading operations across this SeqDF
+	 */
+	def leadingWindow(size: Long, includesNow: Boolean = false): WindowSpec =
+		if(includesNow) {
+			assert( size > 1 )
+			rowsBetweenWindow(Window.currentRow,(size-1))
+		} else {
+			assert( size > 0 )
+			rowsBetweenWindow(1,size)
+		}
+
+	/**
+	 * Construct a centered window for this SeqDF
+	 * @param size the size (in number of sequential observations) of the window
+	 * @return a window appropriate for calculating centered operations across this SeqDF
+	 */
+	def centeredWindow(size: Long): WindowSpec =
+	{
+		assert( size > 1 )
+		rowsBetweenWindow( -(0.5 * (size-1)).floor.toLong,
+		                   (0.5 * (size-1)).ceil.toLong )
+	}
+
+	/**
+	 * Construct a trailing window for this SeqDF
+	 * @param size the size (in number of sequential observations) of the window
+	 * @param includesNow whether the window should include the current observation
+	 * @return a window appropriate for calculating trailing operations across this SeqDF
+	 */
+	def trailingWindow(size: Long, includesNow: Boolean = false): WindowSpec =
+		if(includesNow) {
+			assert( size > 1 )
+			rowsBetweenWindow( -(size-1), Window.currentRow)
+		} else {
+			assert( size > 0 )
+			rowsBetweenWindow(-size, -1)
+		}
+}
+
+object SeqDF
+{
+	/**
+	 * The column types valid for a primary sequence column
+	 */
+	final val validSeqTypes = Seq[DataType](ByteType, ShortType, IntegerType, LongType)
+
+	/**
+	 * Default column name for generated composite sequences
+	 */
+	final val GENERATED_TOTAL_ORDERING_COL = "__TOTAL_ORDER"
+
+	/**
+	 * Helper to validate a column
+	 * @param schema
+	 * @param columnRole
+	 * @param allowedTypes
+	 * @param columnName
+	 */
+	private[tempo] def columnValidator(schema: StructType)(columnRole: String, allowedTypes: Seq[DataType])(columnName: String): Unit =
+	{
+		// make sure the column exists
+		val col = schema.fields.find(_.name.toLowerCase == columnName.toLowerCase)
+		assert( col.isDefined, s"DataFrame does not contain a $columnRole column named $columnName" )
+
+		// validate column type
+		if( allowedTypes.nonEmpty ) {
+			val colType = col.get.dataType
+			assert( allowedTypes.contains(colType),
+			        s"Column $columnName of type $colType cannot be used for a $columnRole column!" )
+		}
+	}
+}
+
+/**
+ * Transformer to convert a [[DataFrame]] to a [[SeqDF]]
+ *
+ * @constructor create a new instance of the [[ToSeqDF]] Transformer
+ * @param uid unique ID for this transformer
+ */
+class ToSeqDF(override val uid: String)
+	extends PipelineStage
+{
+	/**
+	 * @constructor create a new instance of the [[ToSeqDF]] Transformer
+	 */
+	def this() =
+		this(Identifiable.randomUID("ToSeqDF"))
+
+	override def copy(extra: ParamMap): PipelineStage = defaultCopy(extra)
+
+	/** Params */
+
+	/** @group param */
+	final val totalOrderingColName: Param[String] = new Param[String](this,
+	                                                                  "totalOrderingColName",
+	                                                                  "Name of the sequence index column")
+	setDefault(totalOrderingColName, SeqDF.GENERATED_TOTAL_ORDERING_COL)
+
+	/** @group param */
+	final val sequencingColNames: StringArrayParam =
+		new StringArrayParam(this,
+		                     "sequencingColNames",
+		                     "Names of columns that define the sequencing of the DataFrame in combination")
+
+	/** @group param */
+	final val partitionColNames: StringArrayParam =
+		new StringArrayParam(this,
+		                     "partitionColNames",
+		                     "Names of the partitioning columns")
+
+	/** @group getParam */
+	final def getTotalOrderingColName: String = $(totalOrderingColName)
+
+	/** @group getParam */
+	final def getSequencingColNames: Array[String] = $(sequencingColNames)
+
+	/** @group getParam */
+	final def getPartitionColNames: Array[String] = $(partitionColNames)
+
+	/** @group setParam */
+	final def setTotalOrderingColName(value: String): ToSeqDF =
+		set(totalOrderingColName, value)
+
+	/** @group setParam */
+	final def setSequencingColNames(values: Array[String]): ToSeqDF =
+		set(sequencingColNames, values)
+
+	/** @group setParam */
+	final def setPartitionColNames(values: Array[String]): ToSeqDF =
+		set(partitionColNames, values)
+
+	/** Transform */
+
+	/**
+	 * Validate and transform the schema
+ 	 * @param schema schema of the input [[DataFrame]]
+	 * @return a modified schema of the output, transformed SeqDF
+	 */
+	override def transformSchema(schema: StructType): StructType =
+	{
+		// get a column validator
+		val columnValidator = SeqDF.columnValidator(schema)_
+
+		// validate the sequence columns
+		if( $(sequencingColNames).nonEmpty )
+			$(sequencingColNames).foreach( columnValidator("sequence",Seq()) )
+		else
+			columnValidator("sequence",SeqDF.validSeqTypes)($(totalOrderingColName))
+
+		// validate the partition columns
+		if( $(partitionColNames).nonEmpty )
+			$(partitionColNames).foreach(columnValidator("partition",Seq()))
+
+		// add generated column if we're constructing one from combined sequence columnss
+		if( $(sequencingColNames).nonEmpty )
+			StructType( schema.fields :+ StructField(SeqDF.GENERATED_TOTAL_ORDERING_COL,LongType) )
+		else // otherwise, we have not modified the schema
+			schema
+	}
+
+	/**
+	 * Transform the [[DataFrame]] into a [[SeqDF]]
+	 * @param df the input [[DataFrame]]
+	 * @return a [[SeqDF]] instance, representing the input [[DataFrame]]
+	 */
+	def transform(df: DataFrame): SeqDF =
+		new SeqDF(df, $(totalOrderingColName), $(sequencingColNames), $(partitionColNames))
+}
--- scala/tempo/src/main/scala/com/databricks/tempo/TSDF.scala
+++ scala/tempo/src/main/scala/com/databricks/tempo/TSDF.scala
@@ -1,27 +1,102 @@
 package com.databricks.tempo
 
-import org.apache.spark.sql.types.{DateType, DoubleType, FloatType, IntegerType, LongType, NumericType, StructField, TimestampType}
-import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.ml.PipelineStage
+import org.apache.spark.ml.param.{Param, ParamMap, StringArrayParam}
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types._
 
-class TSDF(val df: DataFrame,
-           val tsCol: StructField,
-           val partitionCols: Seq[StructField] = Seq() )
+/**
+ * TSDF - the Timeseries DataFrame
+ *
+ * @constructor creates a new instance of [[TSDF]]
+ * @param df the source [[DataFrame]]
+ * @param tsCol the name of the timeseries column
+ * @param sequencingColumns other sequence columns (used to generate the total ordering)
+ * @param partitionCols columns used to distinguish different sequences from one another
+ */
+class TSDF(override val df: DataFrame,
+           val tsCol: String,
+           override val sequencingColumns: Seq[String],
+           override val partitionCols: Seq[String] )
+  extends SeqDF( df, (tsCol +: sequencingColumns), partitionCols)
 {
-  // types for timestamps
-  private val tsColTypes = Seq( IntegerType, LongType, DoubleType, FloatType, TimestampType, DateType )
+  /** Validate the timeseries column */
+  SeqDF.columnValidator(df.schema)("timeseries",TSDF.tsColTypes)(tsCol)
 
-  // validate the timestamp column
-  assert( df.schema.fields.contains(tsCol), s"DataFrame does not contain timeseries column ${tsCol}!" )
-  assert( tsColTypes.contains(tsCol.dataType),
-          s"Timeseries column ${tsCol} is of a type that cannot be used for a time index!")
+  /** Helper Constructors */
 
-  // validate the partition columns
-  partitionCols.foreach( pCol => assert( df.schema.fields.contains(pCol),
-                                         s"DataFrame does not contain partition column ${pCol}") )
+  /**
+   * @constructor creates a new instance of [[TSDF]]
+   * @param df the input [[DataFrame]]
+   * @param tsCol the name of the timeseries column
+   * @param partitionCols columns used to distinguish different sequences from one another
+   */
+  def this(df: DataFrame, tsCol: String, partitionCols: Seq[String]) =
+    this(df, tsCol, Seq(), partitionCols)
+}
 
-  // take a note of the other columns (not timeseries or partition)
-  val otherCols = df.schema.fields.filter(col => (col != tsCol && ! partitionCols.contains(col)))
+object TSDF
+{
+  // valid types for timeseries index
+  final val tsColTypes = Seq( TimestampType, DateType )
+}
 
-  // take note of measure columns (that have a numeric type)
-  val measureCols = otherCols.filter(col => col.dataType.isInstanceOf[NumericType])
+/**
+ * Transformer to convert a [[DataFrame]] to a [[TSDF]]
+ * @param uid
+ */
+class ToTSDF(override val uid: String)
+  extends PipelineStage
+{
+  override def copy(extra: ParamMap): ToTSDF =
+    defaultCopy(extra)
+
+  /** Params */
+
+  /** @group param */
+  final val tsColName: Param[String] = new Param[String](this, "tsColName", "Name of the timeseries index column")
+  setDefault(tsColName, "event_ts")
+
+  /** @group param */
+  final val partitionColNames: StringArrayParam =
+    new StringArrayParam(this,
+                         "partitionColNames",
+                         "Names of the partitioning columns")
+
+  /** @group getParam */
+  final def getTsColName: String = $(tsColName)
+
+  /** @group getParam */
+  final def getPartitionColNames: Array[String] = $(partitionColNames)
+
+  /** @group setParam */
+  final def setTsColName(value: String): ToTSDF =
+    set(tsColName, value)
+
+  /** @group setParam */
+  final def setPartitionColNames(values: Array[String]): ToTSDF =
+    set(partitionColNames, values)
+
+  /** Transform */
+
+  override def transformSchema(schema: StructType): StructType =
+  {
+    // make sure the timeseries column exists and is of the right type
+    val tsCol = schema.find(_.name.toLowerCase == $(tsColName).toLowerCase)
+    assert( tsCol.isDefined, s"The given timeseries column ${$(tsColName)} does not exist in this DataFrame!" )
+    assert( TSDF.tsColTypes.contains(tsCol.get.dataType),
+            s"The given timeseries column ${$(tsColName)} of type ${tsCol.get.dataType} cannot be used as a temporal index!" )
+
+    // validate the partition columns
+    $(partitionColNames).foreach( pCol => assert(schema.fieldNames
+                                                       .map(_.toLowerCase)
+                                                       .contains(pCol.toLowerCase),
+                                                 s"The partition column ${pCol} does not exist in this DataFrame!" ) )
+
+    // all good - return the schema unchanged
+    schema
+  }
+
+  def transform(df: DataFrame): TSDF =
+    new TSDF(df, $(tsColName), $(partitionColNames))
 }
