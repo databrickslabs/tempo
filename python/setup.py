c9298320646aa187a3cca6ec925f7df7a8c4a97f
Guanjie Shen <guanjie.shen@databricks.com>
2022-05-10 14:03:37 -0600

    test: workflow for linter

--- python/setup.py
+++ python/setup.py
@@ -5,31 +5,27 @@ from version import get_latest_git_tag
 build_version = get_latest_git_tag()
 
 # use the contents of the README file as the 'long description' for the package
-with open('./README.md', 'r') as fh:
+with open("./README.md", "r") as fh:
     long_description = fh.read()
 
 #
 # build the package
 #
 setup(
-    name='dbl-tempo',
+    name="dbl-tempo",
     version=build_version,
-    author='Ricardo Portilla, Tristan Nixon, Max Thone, Sonali Guleria',
-    author_email='labs@databricks.com',
-    description='Spark Time Series Utility Package',
+    author="Ricardo Portilla, Tristan Nixon, Max Thone, Sonali Guleria",
+    author_email="labs@databricks.com",
+    description="Spark Time Series Utility Package",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://databrickslabs.github.io/tempo/',
+    long_description_content_type="text/markdown",
+    url="https://databrickslabs.github.io/tempo/",
     packages=find_packages(where=".", include=["tempo"]),
-    install_requires=[
-        'ipython',
-        'pandas',
-        'scipy'
-    ],
+    install_requires=["ipython", "pandas", "scipy"],
     extras_require=dict(tests=["pytest"]),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: Other/Proprietary License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
     ],
 )
