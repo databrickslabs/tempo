d41b934e719cf1ef59aafcdf7a1e698b3e807bf2
Guanjie Shen <75445106+guanjieshen@users.noreply.github.com>
2022-05-17 01:03:14 -0600

    Chore: Add warning when resampling or interpolating for long time horizons (#205)
    * chore: fix doc strings for interpolate
    
    * feat(issue#170): show warning when interpolating/resampling(with fill)
    
    * chore(issue#170): add millisecond support for warning
    
    * chore: clean up imports and tests
    
    * chore: don't supress warnings
    
    * chore: add support for partitions
    
    * chore: add additional metrics + fix test
    
    * chore: fix test
    
    * chore: remove unused import, rename warning message
    
    Co-authored-by: Guanjie Shen <guanjie.shen@databricks.com>

--- python/tempo/utils.py
+++ python/tempo/utils.py
@@ -1,11 +1,14 @@
+from typing import List
 import logging
 import os
-
+import warnings
 from IPython import get_ipython
 from IPython.core.display import HTML
 from IPython.display import display as ipydisplay
 from pandas import DataFrame as pandasDataFrame
 from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.functions import expr, max, min, sum, percentile_approx
+from tempo.resample import checkAllowableFreq, freq_dict
 
 logger = logging.getLogger(__name__)
 PLATFORM = "DATABRICKS" if "DB_HOME" in os.environ.keys() else "NON_DATABRICKS"
@@ -16,6 +19,15 @@ DB_HOME env variable has been chosen and that's because this variable is a speci
 This constant is to ensure the correct behaviour of the show and display methods are called based on the platform 
 where the code is running from. 
 """
+
+
+class ResampleWarning(Warning):
+    """
+    This class is a warning that is raised when the interpolate or resample with fill methods are called.
+    """
+
+    pass
+
 
 def __is_capable_of_html_rendering():
     """
@@ -32,6 +44,77 @@ def __is_capable_of_html_rendering():
             return False  # Other type (?)
     except NameError:
         return False
+
+
+def calculate_time_horizon(
+    df: DataFrame, ts_col: str, freq: str, partition_cols: List[str]
+):
+    # Convert Frequency using resample dictionary
+    parsed_freq = checkAllowableFreq(freq)
+    freq = f"{parsed_freq[0]} {freq_dict[parsed_freq[1]]}"
+
+    # Get max and min timestamp per partition
+    partitioned_df: DataFrame = df.groupBy(*partition_cols).agg(
+        max(ts_col).alias("max_ts"),
+        min(ts_col).alias("min_ts"),
+    )
+
+    # Generate upscale metrics
+    normalized_time_df: DataFrame = (
+        partitioned_df.withColumn("min_epoch_ms", expr("unix_millis(min_ts)"))
+        .withColumn("max_epoch_ms", expr("unix_millis(max_ts)"))
+        .withColumn(
+            "interval_ms",
+            expr(
+                f"unix_millis(cast('1970-01-01 00:00:00.000+0000' as TIMESTAMP) + INTERVAL {freq})"
+            ),
+        )
+        .withColumn(
+            "rounded_min_epoch", expr("min_epoch_ms - (min_epoch_ms % interval_ms)")
+        )
+        .withColumn(
+            "rounded_max_epoch", expr("max_epoch_ms - (max_epoch_ms % interval_ms)")
+        )
+        .withColumn("diff_ms", expr("rounded_max_epoch - rounded_min_epoch"))
+        .withColumn("num_values", expr("(diff_ms/interval_ms) +1"))
+    )
+
+    (
+        min_ts,
+        max_ts,
+        min_value_partition,
+        max_value_partition,
+        p25_value_partition,
+        p50_value_partition,
+        p75_value_partition,
+        total_values,
+    ) = normalized_time_df.select(
+        min("min_ts"),
+        max("max_ts"),
+        min("num_values"),
+        max("num_values"),
+        percentile_approx("num_values", 0.25),
+        percentile_approx("num_values", 0.5),
+        percentile_approx("num_values", 0.75),
+        sum("num_values"),
+    ).first()
+
+    warnings.simplefilter("always", ResampleWarning)
+    warnings.warn(
+        f"""
+            Resample Metrics Warning: 
+                Earliest Timestamp: {min_ts}
+                Latest Timestamp: {max_ts}
+                No. of Unique Partitions: {normalized_time_df.count()}
+                Resampled Min No. Values in Single a Partition: {min_value_partition}
+                Resampled Max No. Values in Single a Partition: {max_value_partition}
+                Resampled P25 No. Values in Single a Partition: {p25_value_partition}
+                Resampled P50 No. Values in Single a Partition: {p50_value_partition}
+                Resampled P75 No. Values in Single a Partition: {p75_value_partition}
+                Resampled Total No. Values Across All Partitions: {total_values}
+        """,
+        ResampleWarning,
+    )
 
 
 def display_html(df):
@@ -51,29 +134,38 @@ def display_unavailable(df):
     """
     This method is called when display method is not available in the environment.
     """
-    logger.error("'display' method not available in this environment. Use 'show' method instead.")
+    logger.error(
+        "'display' method not available in this environment. Use 'show' method instead."
+    )
 
 
 ENV_BOOLEAN = __is_capable_of_html_rendering()
 
 
-if (PLATFORM == "DATABRICKS") and (type(get_ipython()) != type(None)) and ('display' in get_ipython().user_ns.keys()):
-    method = get_ipython().user_ns['display']
+if (
+    (PLATFORM == "DATABRICKS")
+    and (type(get_ipython()) != type(None))
+    and ("display" in get_ipython().user_ns.keys())
+):
+    method = get_ipython().user_ns["display"]
     # Under 'display' key in user_ns the original databricks display method is present
     # to know more refer: /databricks/python_shell/scripts/db_ipykernel_launcher.py
     def display_improvised(obj):
-        if type(obj).__name__ == 'TSDF':
+        if type(obj).__name__ == "TSDF":
             method(obj.df)
         else:
             method(obj)
+
     display = display_improvised
 
 elif ENV_BOOLEAN:
+
     def display_html_improvised(obj):
-        if type(obj).__name__ == 'TSDF':
+        if type(obj).__name__ == "TSDF":
             display_html(obj.df)
         else:
             display_html(obj)
+
     display = display_html_improvised
 
 else:
