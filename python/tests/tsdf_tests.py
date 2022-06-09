cfd3f1b43773e88d89882af5ecfe2c78c4eba3b0
Souvik Pratiher <98817232+souvik-databricks@users.noreply.github.com>
2022-06-09 22:35:42 +0530

    updating the json files for the tests configs of interpolation and tsdf

--- python/tests/tsdf_tests.py
+++ python/tests/tsdf_tests.py
@@ -126,67 +126,12 @@ class ResampleTest(SparkTest):
 
     def test_resample(self):
         """Test of range stats for 20 minute rolling window"""
-        schema = StructType([StructField("symbol", StringType()),
-                             StructField("date", StringType()),
-                             StructField("event_ts", StringType()),
-                             StructField("trade_pr", FloatType()),
-                             StructField("trade_pr_2", FloatType())])
-
-        expectedSchema = StructType([StructField("symbol", StringType()),
-                                     StructField("event_ts", StringType()),
-                                     StructField("floor_trade_pr", FloatType()),
-                                     StructField("floor_date", StringType()),
-                                     StructField("floor_trade_pr_2", FloatType())])
-
-        expected_30m_Schema = StructType([StructField("symbol", StringType()),
-                                          StructField("event_ts", StringType(), True),
-                                          StructField("date", DoubleType()),
-                                          StructField("trade_pr", DoubleType()),
-                                          StructField("trade_pr_2", DoubleType())])
-
-        expectedBarsSchema = StructType([StructField("symbol", StringType()),
-                                         StructField("event_ts", StringType()),
-                                         StructField("close_trade_pr", FloatType()),
-                                         StructField("close_trade_pr_2", FloatType()),
-                                         StructField("high_trade_pr", FloatType()),
-                                         StructField("high_trade_pr_2", FloatType()),
-                                         StructField("low_trade_pr", FloatType()),
-                                         StructField("low_trade_pr_2", FloatType()),
-                                         StructField("open_trade_pr", FloatType()),
-                                         StructField("open_trade_pr_2", FloatType())])
-
-        data = [["S1", "SAME_DT", "2020-08-01 00:00:10", 349.21, 10.0],
-                ["S1", "SAME_DT", "2020-08-01 00:00:11", 340.21, 9.0],
-                ["S1", "SAME_DT", "2020-08-01 00:01:12", 353.32, 8.0],
-                ["S1", "SAME_DT", "2020-08-01 00:01:13", 351.32, 7.0],
-                ["S1", "SAME_DT", "2020-08-01 00:01:14", 350.32, 6.0],
-                ["S1", "SAME_DT", "2020-09-01 00:01:12", 361.1, 5.0],
-                ["S1", "SAME_DT", "2020-09-01 00:19:12", 362.1, 4.0]]
-
-        expected_data = [
-            ["S1", "2020-08-01 00:00:00", 349.21, "SAME_DT", 10.0],
-            ["S1", "2020-08-01 00:01:00", 353.32, "SAME_DT", 8.0],
-            ["S1", "2020-09-01 00:01:00", 361.1, "SAME_DT", 5.0],
-            ["S1", "2020-09-01 00:19:00", 362.1, "SAME_DT", 4.0]]
-
-        expected_data_30m = [
-            ["S1", "2020-08-01 00:00:00", None, 348.88, 8.0],
-            ["S1", "2020-09-01 00:00:00", None, 361.1, 5.0],
-            ["S1", "2020-09-01 00:15:00", None, 362.1, 4.0]
-        ]
-
-        expected_bars = [
-            ['S1', '2020-08-01 00:00:00', 340.21, 9.0, 349.21, 10.0, 340.21, 9.0, 349.21, 10.0],
-            ['S1', '2020-08-01 00:01:00', 350.32, 6.0, 353.32, 8.0, 350.32, 6.0, 353.32, 8.0],
-            ['S1', '2020-09-01 00:01:00', 361.1, 5.0, 361.1, 5.0, 361.1, 5.0, 361.1, 5.0],
-            ['S1', '2020-09-01 00:19:00', 362.1, 4.0, 362.1, 4.0, 362.1, 4.0, 362.1, 4.0]
-        ]
 
         # construct dataframes
-        df = self.buildTestDF(schema, data)
-        dfExpected = self.buildTestDF(expectedSchema, expected_data)
-        expected_30s_df = self.buildTestDF(expected_30m_Schema, expected_data_30m)
-        barsExpected = self.buildTestDF(expectedBarsSchema, expected_bars)
+        df = self.get_data_as_sdf('input_data')
+        dfExpected = self.get_data_as_sdf('expected')
+        expected_30s_df = self.get_data_as_sdf('expected30m')
+        barsExpected = self.get_data_as_sdf('expectedbars')
 
         # convert to TSDF
         tsdf_left = TSDF(df, partition_cols=["symbol"])
@@ -208,37 +153,10 @@ class ResampleTest(SparkTest):
 
     def test_resample_millis(self):
         """Test of resampling for millisecond windows"""
-        schema = StructType([StructField("symbol", StringType()),
-                             StructField("date", StringType()),
-                             StructField("event_ts", StringType()),
-                             StructField("trade_pr", FloatType()),
-                             StructField("trade_pr_2", FloatType())])
-
-        expectedSchema = StructType([StructField("symbol", StringType()),
-                                     StructField("event_ts", StringType()),
-                                     StructField("floor_trade_pr", FloatType()),
-                                     StructField("floor_date", StringType()),
-                                     StructField("floor_trade_pr_2", FloatType())])
-
-        expectedSchemaMS = StructType([StructField("symbol", StringType()),
-                                          StructField("event_ts", StringType(), True),
-                                          StructField("date", DoubleType()),
-                                          StructField("trade_pr", DoubleType()),
-                                          StructField("trade_pr_2", DoubleType())])
-
-
-        data = [["S1", "SAME_DT", "2020-08-01 00:00:10.12345", 349.21, 10.0],
-                ["S1", "SAME_DT", "2020-08-01 00:00:10.123", 340.21, 9.0],
-                ["S1", "SAME_DT", "2020-08-01 00:00:10.124", 353.32, 8.0]]
-
-        expected_data_ms = [
-            ["S1", "2020-08-01 00:00:10.123", None, 344.71, 9.5],
-            ["S1", "2020-08-01 00:00:10.124", None, 353.32, 8.0]
-        ]
 
         # construct dataframes
-        df = self.buildTestDF(schema, data)
-        dfExpected = self.buildTestDF(expectedSchemaMS, expected_data_ms)
+        df = self.get_data_as_sdf('data')
+        dfExpected = self.get_data_as_sdf('expectedms')
 
         # convert to TSDF
         tsdf_left = TSDF(df, partition_cols=["symbol"])
@@ -253,68 +171,12 @@ class ResampleTest(SparkTest):
 
     def test_upsample(self):
         """Test of range stats for 20 minute rolling window"""
-        schema = StructType([StructField("symbol", StringType()),
-                             StructField("date", StringType()),
-                             StructField("event_ts", StringType()),
-                             StructField("trade_pr", FloatType()),
-                             StructField("trade_pr_2", FloatType())])
-
-        expectedSchema = StructType([StructField("symbol", StringType()),
-                                     StructField("event_ts", StringType()),
-                                     StructField("floor_trade_pr", FloatType()),
-                                     StructField("floor_date", StringType()),
-                                     StructField("floor_trade_pr_2", FloatType())])
-
-        expected_30m_Schema = StructType([StructField("symbol", StringType()),
-                                          StructField("event_ts", StringType()),
-                                          StructField("date", DoubleType()),
-                                          StructField("trade_pr", DoubleType()),
-                                          StructField("trade_pr_2", DoubleType())])
-
-        expectedBarsSchema = StructType([StructField("symbol", StringType()),
-                                         StructField("event_ts", StringType()),
-                                         StructField("close_trade_pr", FloatType()),
-                                         StructField("close_trade_pr_2", FloatType()),
-                                         StructField("high_trade_pr", FloatType()),
-                                         StructField("high_trade_pr_2", FloatType()),
-                                         StructField("low_trade_pr", FloatType()),
-                                         StructField("low_trade_pr_2", FloatType()),
-                                         StructField("open_trade_pr", FloatType()),
-                                         StructField("open_trade_pr_2", FloatType())])
-
-        data = [["S1", "SAME_DT", "2020-08-01 00:00:10", 349.21, 10.0],
-                ["S1", "SAME_DT", "2020-08-01 00:00:11", 340.21, 9.0],
-                ["S1", "SAME_DT", "2020-08-01 00:01:12", 353.32, 8.0],
-                ["S1", "SAME_DT", "2020-08-01 00:01:13", 351.32, 7.0],
-                ["S1", "SAME_DT", "2020-08-01 00:01:14", 350.32, 6.0],
-                ["S1", "SAME_DT", "2020-09-01 00:01:12", 361.1, 5.0],
-                ["S1", "SAME_DT", "2020-09-01 00:19:12", 362.1, 4.0]]
-
-        expected_data = [
-            ["S1", "2020-08-01 00:00:00", 349.21, "SAME_DT", 10.0],
-            ["S1", "2020-08-01 00:01:00", 353.32, "SAME_DT", 8.0],
-            ["S1", "2020-09-01 00:01:00", 361.1, "SAME_DT", 5.0],
-            ["S1", "2020-09-01 00:19:00", 362.1, "SAME_DT", 4.0]]
-
-        expected_data_30m = [
-            ["S1", "2020-08-01 00:00:00", 0.0, 348.88, 8.0],
-            ["S1", "2020-08-01 00:05:00", 0.0, 0.0, 0.0],
-            ["S1", "2020-09-01 00:00:00", 0.0, 361.1, 5.0],
-            ["S1", "2020-09-01 00:15:00", 0.0, 362.1, 4.0]
-        ]
-
-        expected_bars  = [
-            ['S1', '2020-08-01 00:00:00', 340.21, 9.0, 349.21, 10.0, 340.21, 9.0, 349.21, 10.0],
-            ['S1', '2020-08-01 00:01:00', 350.32, 6.0, 353.32, 8.0, 350.32, 6.0, 353.32, 8.0],
-            ['S1', '2020-09-01 00:01:00', 361.1, 5.0, 361.1, 5.0, 361.1, 5.0, 361.1, 5.0],
-            ['S1', '2020-09-01 00:19:00', 362.1, 4.0, 362.1, 4.0, 362.1, 4.0, 362.1, 4.0]
-        ]
-
+        
         # construct dataframes
-        df = self.buildTestDF(schema, data)
-        dfExpected = self.buildTestDF(expectedSchema, expected_data)
-        expected_30s_df = self.buildTestDF(expected_30m_Schema, expected_data_30m)
-        barsExpected = self.buildTestDF(expectedBarsSchema, expected_bars)
+        df = self.get_data_as_sdf('input_data')
+        dfExpected = self.get_data_as_sdf('expected')
+        expected_30s_df = self.get_data_as_sdf('expected30m')
+        barsExpected = self.get_data_as_sdf('expectedbars')
 
         # convert to TSDF
         tsdf_left = TSDF(df, partition_cols=["symbol"])
