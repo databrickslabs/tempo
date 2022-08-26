3c678b6aeee419ea74bec2b324dc875a3342fd76
R7L208 <22798188+R7L208@users.noreply.github.com>
2022-08-26 15:36:39 -0400

    updates to utils for coverage

--- python/tests/utils_tests.py
+++ python/tests/utils_tests.py
@@ -1,3 +1,5 @@
+from io import StringIO
+import sys
 import unittest
 
 from tempo.utils import *
@@ -7,6 +9,9 @@ from unittest import mock
 
 class UtilsTest(SparkTest):
     def test_display(self):
+        # TODO: we shouldn't have conditions in test cases
+        #  the first and second branch are never reached when testing
+        #  split out & mock appropriately to test each
         """Test of the display utility"""
         if IS_DATABRICKS:
             self.assertEqual(id(display), id(display_improvised))
@@ -42,6 +47,95 @@ class UtilsTest(SparkTest):
                 Resampled Total No. Values Across All Partitions: 31.0
             """
             assert warning_message.strip() == str(w[-1].message).strip()
+
+    def test_display_html_TSDF(self):
+        init_tsdf = self.get_data_as_tsdf("init")
+
+        with self.assertLogs(level="ERROR") as error_captured:
+            display_html(init_tsdf)
+
+        self.assertEqual(len(error_captured.records), 1)
+        self.assertEqual(
+            error_captured.output,
+            ["ERROR:tempo.utils:" "'display' method not available for this object"],
+        )
+
+    def test_display_html_dataframe(self):
+        init_tsdf = self.get_data_as_tsdf("init")
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        display_html(init_tsdf.df)
+        self.assertEqual(
+            captured_output.getvalue(),
+            (
+                "<IPython.core.display.HTML object>\n"
+                "+------+-------------------+--------+\n"
+                "|symbol|event_ts           |trade_pr|\n"
+                "+------+-------------------+--------+\n"
+                "|S1    |2020-08-01 00:00:10|349.21  |\n"
+                "|S1    |2020-08-01 00:01:12|351.32  |\n"
+                "|S1    |2020-09-01 00:02:10|361.1   |\n"
+                "|S1    |2020-09-01 00:19:12|362.1   |\n"
+                "|S2    |2020-08-01 00:01:10|743.01  |\n"
+                "|S2    |2020-08-01 00:01:24|751.92  |\n"
+                "|S2    |2020-09-01 00:02:10|761.1   |\n"
+                "|S2    |2020-09-01 00:20:42|762.33  |\n"
+                "+------+-------------------+--------+\n"
+                "\n"
+            ),
+        )
+
+    def test_display_html_pandas_dataframe(self):
+        init_tsdf = self.get_data_as_tsdf("init")
+        pandas_dataframe = init_tsdf.df.toPandas()
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        display_html(pandas_dataframe)
+        self.assertEqual(
+            captured_output.getvalue(),
+            (
+                "<IPython.core.display.HTML object>\n"
+                "  symbol            event_ts    trade_pr\n"
+                "0     S1 2020-08-01 00:00:10  349.209991\n"
+                "1     S1 2020-08-01 00:01:12  351.320007\n"
+                "2     S1 2020-09-01 00:02:10  361.100006\n"
+                "3     S1 2020-09-01 00:19:12  362.100006\n"
+                "4     S2 2020-08-01 00:01:10  743.010010\n"
+            ),
+        )
+
+    def test_display_unavailable(self):
+        init_tsdf = self.get_data_as_tsdf("init")
+
+        with self.assertLogs(level="ERROR") as error_captured:
+            display_unavailable(init_tsdf)
+
+        self.assertEqual(len(error_captured.records), 1)
+        self.assertEqual(
+            error_captured.output,
+            [
+                "ERROR:tempo.utils:"
+                "'display' method not available in this environment. Use 'show' method instead."
+            ],
+        )
+
+    def test_get_display_df(self):
+        init_tsdf = self.get_data_as_tsdf("init")
+        expected_df = self.get_data_as_sdf("expected")
+
+        actual_df = get_display_df(init_tsdf, 2)
+
+        self.assertDataFrameEquality(actual_df, expected_df)
+
+    def test_get_display_df_sequence_col(self):
+        init_tsdf = self.get_data_as_tsdf("init")
+        expected_df = self.get_data_as_sdf("expected")
+
+        actual_df = get_display_df(init_tsdf, 2)
+
+        self.assertDataFrameEquality(actual_df, expected_df)
 
 
 # MAIN
