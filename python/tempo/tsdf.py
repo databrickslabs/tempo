6708ce55a12e337a39b2ff4a2ccdb672afa36b79
Lorin <lorin.dawson@databricks.com>
2024-10-01 13:01:33 -0600

    add indentation to newline in params so doc-utils recognizes it as param definiton

--- python/tempo/tsdf.py
+++ python/tempo/tsdf.py
@@ -389,7 +389,7 @@ class TSDF:
         pyspark.sql.DataFrame.select() method's equivalent for TSDF objects
 
         :param cols: str or list of strs column names (string). If one of the column names is '*', that
-        column is expanded to include all columns in the current :class:`TSDF`.
+            column is expanded to include all columns in the current :class:`TSDF`.
 
         ## Examples
         .. code-block:: python
@@ -582,8 +582,8 @@ class TSDF:
 
         :param n: Number of rows to show. (default: 20)
         :param truncate: If set to True, truncate strings longer than 20 chars by default.
-        If set to a number greater than one, truncates long strings to length truncate
-        and align cells right.
+            If set to a number greater than one, truncates long strings to length truncate
+            and align cells right.
         :param vertical: If set to True, print output rows vertically (one line per column value).
 
         ## Example to show usage:
@@ -604,7 +604,7 @@ class TSDF:
         # Call show method here
         phone_accel_tsdf.show()
         """
-        
+
         # validate k <= n
         if k > n:
             raise ValueError(f"Parameter k {k} cannot be greater than parameter n {n}")
