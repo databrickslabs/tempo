609e8654642f28f9688a8c87aae7451f1c70d5cc
Lorin <lorin.dawson@databricks.com>
2025-03-20 11:15:17 -0600

    fixing type errors for timeunit.py

--- python/tempo/timeunit.py
+++ python/tempo/timeunit.py
@@ -1,5 +1,5 @@
 from functools import total_ordering
-from typing import NamedTuple
+from typing import NamedTuple, Any
 
 
 @total_ordering
@@ -11,10 +11,14 @@ class TimeUnit(NamedTuple):
     and the approximate number of seconds in that unit.
     """
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, TimeUnit):
+            return NotImplemented
         return self.approx_seconds == other.approx_seconds
 
-    def __lt__(self, other):
+    def __lt__(self, other: object) -> bool:
+        if not isinstance(other, TimeUnit):
+            return NotImplemented
         return self.approx_seconds < other.approx_seconds
 
 
