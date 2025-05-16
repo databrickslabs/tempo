fa11990b0393c4aa3cbe309e0968a62c70fc51ae
Kevin Wang <k.wang@databricks.com>
2025-05-16 13:37:23 -0700

    fixing correct error type

--- python/version.py
+++ python/version.py
@@ -21,7 +21,7 @@ def get_latest_git_tag():
     try:
         import semver
         semver.VersionInfo.parse(build_version)
-    except NameError as name_error:
+    except (ModuleNotFoundError) as module_not_found_error:
         # unable to validate because semver is not installed in barebones env for hatch
         pass
     return build_version
