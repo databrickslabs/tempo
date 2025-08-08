c6272b7a3d3fe9bb055501bb9ceb369260fbb09a
Lorin <lorin.dawson@databricks.com>
2025-08-08 09:29:58 -0600

    Revert "Add interval module implementation from commit 2cfe796"
    This reverts commit 9ac26446f2b6253f0890f302c212aac54b6ffcc7.

--- python/python/tempo/intervals/core/types.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from typing import TypeVar, Union
-
-from pandas import Timestamp
-
-IntervalBoundary = Union[str, int, float, Timestamp, datetime, None]
-MetricValue = Union[int, float, bool]
-T = TypeVar("T")
